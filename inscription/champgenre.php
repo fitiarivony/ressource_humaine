3e46e745a336e29ffb25dfae478b17a0a0e733ad
fitiarivony <fitiarivony2002@gmail.com>
2022-09-30 22:50:27 +0300

    Update champgenre.php

--- inscription/champgenre.php
+++ inscription/champgenre.php
@@ -2,8 +2,8 @@
 header('Access-Control-Allow-Origin:http://localhost:3000');
 include ('../connection.php');
 
-$sql="SELECT * from champ where idchamp!='CH1' and idchamp!='CH4'";
-
+$sql="SELECT * from champ where idchamp!='CH1' and idchamp!='CH4' and idrecrutement='%s'";
+$sql=sprintf($sql,$_GET['idrecrutement']);
 
 $result=executeQuery($sql);
 echo json_encode($result);
