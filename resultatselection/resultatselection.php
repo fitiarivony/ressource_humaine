1c56af466ca47bc77970b3d9b883daf9dd6a3355
fitiarivony <fitiarivony2002@gmail.com>
2022-09-30 21:09:29 +0300

    Update resultatselection.php

--- resultatselection/resultatselection.php
+++ resultatselection/resultatselection.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin:http://localhost:3000');
 include ('../connection.php');
 
-$sql="SELECT * from resultat_selection join candidat on resultat_selection.idcandidat=candidat.idcandidat  join recrutement on resultat_selection.idrecrutement=recrutement.idrecrutement where idrecrutement='%s' and datelimite<current_timestamp";
+$sql="SELECT * from resultat_selection join candidat on resultat_selection.idcandidat=candidat.idcandidat  join recrutement on resultat_selection.idrecrutement=recrutement.idrecrutement where resultat_selection.idrecrutement='%s' and datelimite<current_timestamp";
 $sql=sprintf($sql,$_GET['idrecrutement']);
 // echo $sql;
 $result=executeQuery($sql);
